8069dc69230408fdddae8dfed51759c1aa2e64a1
fernando23296 <fernando23296@gmail.com>
2019-12-16 11:02:51 -0400

    variables

---
+++ swift_tuto1/03.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let age : UInt8
+age = 31
+
+//numero min y max de numeros q abarca 8 bits
+let minValueint8 = UInt8.min //2**0-1
+let maxValueint8 = UInt8.max //2**8-1
+//numero min y max de numeros q abarca 16 bits
+let minValueint16 = UInt16.min
+let maxValueint16 = UInt16.max
+
+//numero min y max de numeros q abarca 32 bits
+let minValueint32 = UInt32.min
+let maxValueint32 = UInt32.max
+
+//numero min y max de numeros q abarca 64 bits
+let minValueint64 = UInt64.min
+let maxValueint64 = UInt64.max
+
+//para guardar una edad
+
