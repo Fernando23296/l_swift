76ebdfc34e6adaa23121a01772fd9f1951aa06ae
fernando23296 <fernando23296@gmail.com>
2019-12-16 10:36:03 -0400

    Fcommit

---
+++ swift_tuto1/02_vars.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let numMaximoLogin = 3 //usar let para constantes
+
+var currentLoginAttempt = 0 //numero de intentos, usar var para variables
+
+//se puede declarar muchas variables y constantes en una linea, separandolos con una coma
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage : String //es una variable que algun momento sera String
+
+welcomeMessage = "Hola"
+
+var red,green,blue : Double
+
+// concatenacion
+print("el numero de intentos es:\(currentLoginAttempt) de un total disponible de: \(numMaximoLogin)")
+
---
+++ swiftuno.playground/Pages/variables_constantes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// para definir que es una variable
+// usaremos camelcase
+var myFirstVariable = "hola ahi"
+var myFirstNumber = 9
+
+print(myFirstVariable)
+
+myFirstVariable = "que tal"
+
+print(myFirstVariable)
+
+/*
+la variable es inmutable
+ porque empezo como string
+myFirstVariable = 2
+print(myFirstVariable)
+ */
+
+var mySecondVariable = "hello hackerman"
+print(mySecondVariable)
+
+mySecondVariable = myFirstVariable
+print(mySecondVariable)
+
+// CONSTANTES
+
+let myFirstConstant = "aloha"
+print(myFirstConstant)
+
+//No puede ser modificado, por ser constante
+//myFirstConstant = "olo"
+//print(myFirstConstant)
+
